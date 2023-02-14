d0494264b00515e3784e76f2bb8ee7cc61fb8c34
Auggie Marignier <augustin.marignier@anu.edu.au>
2023-02-14 12:46:42 +1100

    main delay times function

--- aussedthickrf/delay_times.py
+++ aussedthickrf/delay_times.py
@@ -12,55 +12,59 @@ from collections import defaultdict
 
 from utils import plot_stacks
 
-# Load precomputed RFs for a network
-RF_FILENAME = sys.argv[1]
-rf_stream_master = rf.read_rf(RF_FILENAME)
-basename = os.path.splitext(RF_FILENAME)[0]
 
-# Drop RFs that do not meet quality estimate
-MIN_SLOPE_RATIO = 5
-rf_stream = rf.RFStream([tr.copy() for tr in rf_stream_master
-                        if tr.stats.slope_ratio > MIN_SLOPE_RATIO]).sort(['back_azimuth'])
-rf_stream = rf_stream.select(channel='??R')
+def main(rf_filename):
+    # Load precomputed RFs for a network
+    rf_stream_master = rf.read_rf(rf_filename)
+    basename = os.path.splitext(rf_filename)[0]
 
-# Create a dictionary of RFs by station
-rf_station_dict = defaultdict(list)
-for trc in rf_stream:
-    rf_station_dict[trc.stats.station].append(trc)
+    # Drop RFs that do not meet quality estimate
+    MIN_SLOPE_RATIO = 5
+    rf_stream = rf.RFStream([tr.copy() for tr in rf_stream_master
+                            if tr.stats.slope_ratio > MIN_SLOPE_RATIO]).sort(['back_azimuth'])
+    rf_stream = rf_stream.select(channel='??R')
 
-# Trim RFs
-for k, v in rf_station_dict.items():
-    temp_stream = rf.RFStream([tr.copy() for tr in v])
-    rf_station_dict[k] = temp_stream.trim2(-5, 10, reftime='onset')
+    # Create a dictionary of RFs by station
+    rf_station_dict = defaultdict(list)
+    for trc in rf_stream:
+        rf_station_dict[trc.stats.station].append(trc)
 
-# Moveout, stack and compute delays
-stacked = []
-delays = []
-for k, v in rf_station_dict.items():
-    m = v.copy().moveout()
-    try:
-        # sometimes this fails
-        # moveout might produce some weird numpy arrays that can't be stacked
-        s = m.stack()[0]
-    except ValueError:
-        continue
-    else:
-        d = s.times()[np.argmax(s.data)] - (s.stats.onset - s.stats.starttime)
-        s.stats['delay'] = d
-        stacked.append(s)
-        delays.append(d)
-stacked = rf.RFStream(stacked).sort(['delay'], reverse=True)
+    # Trim RFs
+    for k, v in rf_station_dict.items():
+        temp_stream = rf.RFStream([tr.copy() for tr in v])
+        rf_station_dict[k] = temp_stream.trim2(-5, 10, reftime='onset')
+
+    # Moveout, stack and compute delays
+    stacked = []
+    delays = []
+    for k, v in rf_station_dict.items():
+        m = v.copy().moveout()
+        try:
+            # sometimes this fails
+            # moveout might produce some weird numpy arrays that can't be stacked
+            s = m.stack()[0]
+        except ValueError:
+            continue
+        else:
+            d = s.times()[np.argmax(s.data)] - (s.stats.onset - s.stats.starttime)
+            s.stats['delay'] = d
+            stacked.append(s)
+            delays.append(d)
+    stacked = rf.RFStream(stacked).sort(['delay'], reverse=True)
+
+    # Save delays
+    stations_with_delays = {}
+    with open(f"{basename}_delays.txt", "w") as f:
+        for tr in stacked:
+            stations_with_delays[tr.meta.station] = tr.stats.delay
+            f.write(f"{tr.meta.station:<8}\t{tr.stats.delay:.2}\n")
+
+    return stations_with_delays
+
 
 # Plot stacks
 fig = plot_stacks(stacked)
 fig.savefig(f"{basename}_delays.png")
-
-# Save delays
-stations_with_delays = {}
-with open(f"{basename}_delays.txt", "w") as f:
-    for tr in stacked:
-        stations_with_delays[tr.meta.station] = tr.stats.delay
-        f.write(f"{tr.meta.station:<8}\t{tr.stats.delay:.2}\n")
 
 
 # Plot map
