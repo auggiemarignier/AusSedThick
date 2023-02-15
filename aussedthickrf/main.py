0afc2044f7f8e41c7d6f4522a688b8b09439fe71
Auggie Marignier <augustin.marignier@anu.edu.au>
2023-02-15 20:44:59 +1100

    setup plotting funcs

--- aussedthickrf/main.py
+++ aussedthickrf/main.py
@@ -2,6 +2,7 @@ from seismic import extract_event_traces
 from seismic.receiver_fn import generate_rf, bulk_rf_report, rf_quality_filter
 import delay_times
 from utils import signoise, parse_config
+from plotting import plot_stacks, plot_map
 
 import rf
 from os.path import splitext, expanduser
@@ -106,3 +107,5 @@ bulk_rf_report.main(
     network_list=config["download_waveforms"]["network_list"],
     station_list=config["download_waveforms"]["station_list"],
 )
+plot_stacks()
+plot_map()
